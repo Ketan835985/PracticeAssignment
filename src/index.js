b9d71676901d5d1545a0a929fb980683177dc96d
Ketan Gupta <guptaketan6375@gmail.com>
2023-05-19 21:40:52 +0530

    Assignment Done!

--- src/index.js
+++ src/index.js
@@ -11,15 +11,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.use (
+app.use(
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+        //2010-08-19 14:00:00 , 123.459.898.734 , /createUser
+        try {
+            const date = new Date();
+            const ip = req.ip
+            const url = req.url
+            console.log(`${date.toLocalString()}, ${ip}, ${url}`)
+            next();
+        }
+        catch (err) {
+            res.status(400).send({ message: err.message })
+        }
+    }
+);
 
 app.use('/', route);
 
